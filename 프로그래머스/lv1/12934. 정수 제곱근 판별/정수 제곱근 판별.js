74ece532135a862aa13d80bd6786347dfd4f7b7f
Suziech <99739613+Suziech@users.noreply.github.com>
2022-09-25 13:45:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let defaultValue = -1
+for(let i=1; i<n+1; i++){
+    if(i**2 === n){      
+        return defaultValue = (i+1) ** 2;    
+        }
+    }
+    return defaultValue
+    }
+
+
