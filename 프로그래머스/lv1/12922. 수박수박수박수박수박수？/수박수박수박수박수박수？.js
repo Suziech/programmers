1d84f3cdf404801999067e10d19a450246314ddf
Suzie <99739613+Suziech@users.noreply.github.com>
2022-10-06 21:38:48 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,8 @@
 const solution = n => {
     let result = "";
-for(let i=1; i<(n+1); i++){
-    if(i%2){
-        result += "수"
-    } else{
-        result += "박"
+    for(let i=1; i<(n+1); i++){
+    result += (i%2) ? "수" : "박"
     }
-}
     return result;
 }
+
