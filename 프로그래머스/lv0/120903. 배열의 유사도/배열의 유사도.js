9b86cccaa5920ecb4d0d0cc2bebcce40cc140bae
Suzie <99739613+Suziech@users.noreply.github.com>
2022-10-31 11:41:47 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,7 +1,9 @@
-const solution = (s1, s2) => {
-    const result = [];
-    for(let i=0; i<s2.length; i++){
-        result.push(s1.filter(ele => ele === s2[i]))
-    }
-    return result.flat().length;
-}
+// const solution = (s1, s2) => {
+//     const result = [];
+//     for(let i=0; i<s2.length; i++){
+//         result.push(s1.filter(ele => ele === s2[i]))
+//     }
+//     return result.flat().length;
+// }
+
+const solution = (s1, s2) => s1.filter(x => s2.includes(x)).length;
