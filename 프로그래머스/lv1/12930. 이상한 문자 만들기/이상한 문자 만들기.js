24c3e69a810247163cc42772ae5794d6e9245c41
Suzie <99739613+Suziech@users.noreply.github.com>
2022-11-02 11:44:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,19 +1,11 @@
 const solution = s => {
-const arr = s.split(' ')
-const splittedArr = [];
-for(let i=0; i<arr.length; i++){
-  splittedArr.push(arr[i].split(''))
-}
-const result = []
-for(let i = 0; i < splittedArr.length; i++){
-  for(let j = 0; j < splittedArr[i].length; j++){
-    if(j%2){
-      splittedArr[i][j] = splittedArr[i][j].toLowerCase()
-    } else {
-      splittedArr[i][j] = splittedArr[i][j].toUpperCase()
+    const splittedArr = s.split(' ').map(ele => ele.split(''));
+    let result = [];
+    for(let i=0; i<splittedArr.length; i++) {
+        for(let j=0; j<splittedArr[i].length; j++){
+            splittedArr[i][j] = j%2 ? splittedArr[i][j].toLowerCase() : splittedArr[i][j].toUpperCase()
+        }
+        result.push(splittedArr[i].join(''))
     }
-  }
-  result.push(splittedArr[i].join(''))
-}
     return result.join(' ')
 }
