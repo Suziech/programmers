42695cb2921bbbc509a30f6d64c16b8637de8fea
Suzie <99739613+Suziech@users.noreply.github.com>
2022-11-17 13:58:14 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,12 +1,4 @@
-function solution(age) {
-const ageDictionary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-
-const arr = String(age).split('')
-
-let name = ''
-for(let i=0; i<arr.length; i++){
-  name += ageDictionary[arr[i]]
-}
-    return name
-
-}
+const solution = age => {
+    const alphabet = 'abcdefghij'
+    return String(age).split('').map(item => alphabet[item]).join('')
+} 
