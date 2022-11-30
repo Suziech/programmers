4a163647418cef7607eb127412fcb3725a74f9bb
Suzie <99739613+Suziech@users.noreply.github.com>
2022-11-30 11:12:58 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -2,5 +2,3 @@ const solution = (before, after) => {
     const func = a => a.split('').sort().join('');
     return func(before) === func(after) ? 1 : 0
 }
-
-// before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0
