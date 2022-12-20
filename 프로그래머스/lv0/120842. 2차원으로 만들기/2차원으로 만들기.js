8ce93b511d2417af7d57fc95a1416e5bf8c18d1b
Suzie <99739613+Suziech@users.noreply.github.com>
2022-12-20 11:14:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,7 @@
 const solution = (num_list, n) => {
     let arr = [];
     for(let i=0; i<num_list.length/n; i++){
-        arr = [...arr, num_list.slice(i*n, i*n+n)]
+        arr = [...arr, num_list.slice(n*i, n*i+n)]
     }
-    return arr
+    return arr;
 }
