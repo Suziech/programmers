a6b991c24c43d366744409cac3fd68af1fd16202
Suzie <99739613+Suziech@users.noreply.github.com>
2023-10-25 11:23:50 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 2.41 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    queries.forEach(query => {
+        const [start, end] = query;
+        
+        for(let i=start; i<=end; i++){
+            arr[i] += 1
+        }
+    })
+    return arr
+}
