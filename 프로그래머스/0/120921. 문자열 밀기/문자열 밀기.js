05946bb0482dbf8a180396208b21ca31046b1c87
Suzie <99739613+Suziech@users.noreply.github.com>
2024-11-18 09:25:08 +0900

    [level 0] Title: 문자열 밀기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,4 @@
 function solution(A, B) {
-    for(let i=0; i<A.length; i++){
-        if(A === B) {
-            return i
-        } 
-        
-        A = A.slice(-1) + A.slice(0,A.length-1)
-    }
-    return -1
+    var answer = 0;
+    return answer;
 }
