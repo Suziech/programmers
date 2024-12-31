cc1fb2068073b07fafa218f85d0a33b379263d60
Suzie <99739613+Suziech@users.noreply.github.com>
2024-12-31 09:43:23 +0900

    [level 0] Title: 평행, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -0,0 +1,17 @@
+function solution(dots) {
+    const getSlope = ([x1, y1], [x2, y2]) => {
+        return (y2-y1) / (x2-x1)
+    }
+    
+    const combinations = [[0,1,2,3],[0,2,1,3],[0,3,1,2]];
+    
+    for (const [a,b,c,d] of combinations) {
+        const slope1 = getSlope(dots[a], dots[b]);
+        const slope2 = getSlope(dots[c], dots[d]);
+        
+        if(slope1 === slope2) {
+            return 1
+        }
+    }
+    return 0
+}
